03e4a5e529f8ea43545782309364d96aca3b6bd6
vlabra <vlad.abramovich.28@gmail.com>
2023-04-30 17:49:16 +0000

    Small changes

--- hw1/make_query.py
+++ hw1/make_query.py
@@ -4,4 +4,4 @@ import sys
 args = sys.argv
 assert(len(args) == 1, 'Bad number of args, must be one')
 
-subprocess.run(["curl", "-XGET", "http://localhost:2000/get_result?method=" + args[0]])
+subprocess.run(["curl", "-XGET", "http://localhost:2000/get_result?method=" + args[0].lower()])
